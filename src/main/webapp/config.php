0a19a0ad9380a9ce7148f17476d9b8970bb85dec
rameshkalluri <rrksrb@gmail.com>
2020-01-16 14:27:03 +0530

    Update config.php

--- src/main/webapp/config.php
+++ src/main/webapp/config.php
@@ -1,7 +1,16 @@
 <?php
-   define('DB_SERVER', 'localhost:3036');
-   define('DB_USERNAME', 'root');
-   define('DB_PASSWORD', 'reddy');
-   define('DB_DATABASE', 'reddy');
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'reddy');
+define('DB_NAME', 'reddy');
+ 
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
 ?>
